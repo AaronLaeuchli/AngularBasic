aceea06877ace2c696d38a5b1d87109c50b79ead
Aaron Läuchli <aaron.laeuchli@rahnbodmer.ch>
2021-11-05 13:50:44 +0100

    Basis Projekt

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../Task';
+import { TASKS } from '../mock-tasks';
+
+import { Observable, of } from 'rxjs';
+
+import { HttpClient, HttpHeaders} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]>{
+    //Mit dem of wird der TASKS Array zu einem Observable Array vom Typ Task konvertiert, damit er mitgegeben werden kann
+    //const tasks = of(TASKS);
+    //return tasks;
+
+    //Über den HttpRequest werden die Daten aus meinem Mock-Server importiert (get)
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task>{
+    const urlToDelete = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(urlToDelete);
+  } 
+}
